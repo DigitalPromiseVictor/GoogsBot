0fe87a03e03fc941e519e11afbf20ff1a6785fff
Victor Ngo <vngo@digitalpromise.org>
2018-04-14 01:04:42 -0700

    Scaffolding setup

---
+++ index.js
@@ -0,0 +1,79 @@
+const {google} = require('googleapis');
+const axios = require('axios')
+const express = require('express')
+const app = express();
+const port = 8080
+// Each API may support multiple version. With this sample, we're getting
+// v1 of the urlshortener API, and using an API key to authenticate.
+const secret = require('./client_secret.json')
+
+const oauth2Client = new google.auth.OAuth2(
+    secret.web.client_id,
+    secret.web.client_secret,
+    secret.web.redirect_uris[0]
+  );
+
+  // generate a url that asks permissions for Google+ and Google Calendar scopes
+  const scopes = [
+    'https://www.googleapis.com/auth/plus.me',
+    'https://www.googleapis.com/auth/hangout.av',
+    'https://www.googleapis.com/auth/hangout.participants'
+
+  ];
+  
+  const url = oauth2Client.generateAuthUrl({
+    // 'online' (default) or 'offline' (gets refresh_token)
+    access_type: 'offline',
+  
+    // If you only need one scope you can pass it as a string
+    scope: scopes
+  });
+  
+  app.listen(port, _ => {
+    console.log('Server listening at port number', port)
+  })
+  /*
+Routes
+  */
+  app.get('/', function (req, res) {
+    res.redirect(url);
+  })
+
+  app.get('/redirect', function (req, res) {
+    const {code} = req.query;
+    console.log('Authroizaation code from Google', code)
+    let tokens;
+    
+    oauth2Client.getToken(code)
+        .then(data => {
+            tokens = data.tokens;
+            oauth2Client.setCredentials(tokens);
+            urlShortener();
+        })
+        .catch(err => {
+            console.error('ERROR /redirect', err);
+        })
+    
+    
+  })
+
+
+  function urlShortener() {
+    const urlshortener = google.urlshortener({
+        version: 'v1',
+        auth: secret.api_key
+      });
+      
+      const params = {
+        shortUrl: 'http://goo.gl/xKbRu3'
+      };
+      
+      // get the long url of a shortened url
+      urlshortener.url.get(params, (err, res) => {
+        if (err) {
+          console.error(err);
+          throw err;
+        }
+        console.log(`Long url is ${res.data.longUrl}`);
+      });
+  }
